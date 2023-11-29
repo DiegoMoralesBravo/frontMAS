ff6e0daceb232155899b14caeb68ff584fadc77f
Diego Morales <diegomorales1359@gmail.com>
2023-11-29 08:48:47 -0600

    test

--- src/components/Plants/Plants.jsx
+++ src/components/Plants/Plants.jsx
@@ -1,27 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import plantsData from './api.js'; // Importar los datos de 'api.js'
 
 export const Plants = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [selectedPlants, setSelectedPlants] = useState([]);
 
-  // Datos de prueba ampliados
-  const mockData = [
-    'Rosa', 'Tulipán', 'Girasol', 'Orquídea', 'Margarita', 'Clavel',
-    'Lavanda', 'Azalea', 'Begonia', 'Cala', 'Dalia', 'Ébano',
-    'Fresia', 'Geranio', 'Hortensia', 'Iris', 'Jazmín', 'Kalanchoe',
-    'Lirio', 'Magnolia', 'Narciso', 'Olivo', 'Petunia', 'Quinoa',
-    'Rododendro', 'Salvia', 'Tomillo', 'Uva', 'Verbena', 'Wisteria',
-    'Xerochrysum', 'Yucca', 'Zinnia', 'Aloe', 'Bonsái', 'Cactus',
-    'Drácena', 'Eucalipto', 'Ficus', 'Ginkgo', 'Helecho', 'Ivy',
-    'Junípero', 'Kiri', 'Laurel', 'Mimosa', 'Nogal', 'Olmo',
-    'Pino', 'Quercus', 'Roble', 'Secuoya', 'Tejo', 'Ulmo',
-    'Violeta', 'Weigela', 'Xanthoceras', 'Yew', 'Zamia'
-  ];
-
   const searchPlants = (searchQuery) => {
-    const filteredData = mockData.filter(plant =>
-      plant.toLowerCase().includes(searchQuery.toLowerCase())
+    const filteredData = plantsData.filter(plant =>
+      plant.nombre.toLowerCase().includes(searchQuery.toLowerCase())
     );
     setResults(filteredData);
   };
@@ -59,7 +46,7 @@ export const Plants = () => {
       <div>
         {results.map((plant, index) => (
           <div key={index} className="result-item">
-            {plant}
+            {plant.nombre}
             <button onClick={() => addPlant(plant)}>+</button>
           </div>
         ))}
@@ -69,14 +56,20 @@ export const Plants = () => {
         <table className="crud-table">
           <thead>
             <tr>
-              <th>Nombre de la Planta</th>
+              <th>Nombre</th>
+              <th>Frecuencia de Riego</th>
+              <th>Descripción</th>
+              <th>Recomendaciones</th>
               <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
             {selectedPlants.map((plant, index) => (
               <tr key={index}>
-                <td>{plant}</td>
+                <td>{plant.nombre}</td>
+                <td>{plant.frecuenciaRiego} horas</td>
+                <td>{plant.descripcion}</td>
+                <td>{plant.recomendaciones}</td>
                 <td>
                   <button onClick={() => removePlant(index)}>Eliminar</button>
                 </td>
---
+++ src/components/Plants/api.js
@@ -0,0 +1,34 @@
+[
+    {
+      "nombre": "Aloe Vera",
+      "frecuenciaRiego": 72,
+      "descripcion": "Planta suculenta conocida por sus propiedades medicinales.",
+      "recomendaciones": "Luz solar indirecta y suelo bien drenado."
+    },
+    {
+      "nombre": "Sansevieria (Lengua de Suegra)",
+      "frecuenciaRiego": 168,
+      "descripcion": "Planta resistente con hojas largas y puntiagudas.",
+      "recomendaciones": "Poca agua, tolera luz baja."
+    },
+    {
+      "nombre": "Ficus Benjamina",
+      "frecuenciaRiego": 48,
+      "descripcion": "Árbol de interior popular con hojas brillantes.",
+      "recomendaciones": "Alto nivel de luz, evitar corrientes de aire."
+    },
+    {
+      "nombre": "Helecho de Boston",
+      "frecuenciaRiego": 24,
+      "descripcion": "Planta frondosa con hojas plumosas.",
+      "recomendaciones": "Alta humedad y luz indirecta."
+    },
+    {
+      "nombre": "Monstera Deliciosa",
+      "frecuenciaRiego": 72,
+      "descripcion": "Planta tropical con hojas grandes y perforadas.",
+      "recomendaciones": "Luz indirecta y alta humedad."
+    },
+    // ... (continuar hasta completar 100 objetos)
+  ]
+  
