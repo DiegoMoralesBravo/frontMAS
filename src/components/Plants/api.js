ef64ef7e204cf1ed7200f25232fe425b2a20bf6a
Diego Morales <diegomorales1359@gmail.com>
2023-11-29 08:54:38 -0600

    test

--- src/components/Plants/api.js
+++ src/components/Plants/api.js
@@ -1,4 +1,4 @@
-[
+const plantsData = [
     {
       "nombre": "Aloe Vera",
       "frecuenciaRiego": 72,
@@ -29,6 +29,113 @@
       "descripcion": "Planta tropical con hojas grandes y perforadas.",
       "recomendaciones": "Luz indirecta y alta humedad."
     },
-    // ... (continuar hasta completar 100 objetos)
+    {
+        "nombre": "Potos (Epipremnum aureum)",
+        "frecuenciaRiego": 72,
+        "descripcion": "Enredadera con hojas en forma de corazón, muy adaptable.",
+        "recomendaciones": "Luz indirecta, suelo húmedo."
+      },
+      {
+        "nombre": "Palma Areca",
+        "frecuenciaRiego": 48,
+        "descripcion": "Palmera elegante con hojas plumosas.",
+        "recomendaciones": "Buena luz, evitar el exceso de agua."
+      },
+      {
+        "nombre": "Cactus",
+        "frecuenciaRiego": 336,
+        "descripcion": "Variedad de especies suculentas con espinas.",
+        "recomendaciones": "Mucho sol, riego escaso."
+      },
+      {
+        "nombre": "Hiedra Inglesa",
+        "frecuenciaRiego": 48,
+        "descripcion": "Planta trepadora con hojas verdes brillantes.",
+        "recomendaciones": "Luz moderada, mantener el suelo uniformemente húmedo."
+      },
+      {
+        "nombre": "Planta Araña",
+        "frecuenciaRiego": 72,
+        "descripcion": "Planta con hojas largas y delgadas que caen en cascada.",
+        "recomendaciones": "Luz indirecta, riego moderado."
+      },
+      {
+        "nombre": "Philodendron",
+        "frecuenciaRiego": 72,
+        "descripcion": "Planta con grandes hojas verdes, fácil de cuidar.",
+        "recomendaciones": "Luz indirecta, suelo húmedo pero no empapado."
+      },
+      {
+        "nombre": "Zamioculcas",
+        "frecuenciaRiego": 168,
+        "descripcion": "Planta resistente con hojas brillantes y gruesas.",
+        "recomendaciones": "Poca luz, riego esporádico."
+      },
+      {
+        "nombre": "Bambú de la Suerte",
+        "frecuenciaRiego": 168,
+        "descripcion": "Tallos verdes que crecen en espiral o rectos.",
+        "recomendaciones": "Poca luz, mantener el agua fresca."
+      },
+      {
+        "nombre": "Begonia",
+        "frecuenciaRiego": 72,
+        "descripcion": "Planta con flores vistosas y hojas ornamentales.",
+        "recomendaciones": "Luz indirecta, evitar agua en las hojas."
+      },
+      {
+        "nombre": "Calathea",
+        "frecuenciaRiego": 48,
+        "descripcion": "Planta con hojas grandes y patrones llamativos.",
+        "recomendaciones": "Alta humedad, evitar luz solar directa."
+      },
+      {
+        "nombre": "Peperomia",
+        "frecuenciaRiego": 168,
+        "descripcion": "Pequeña planta ornamental con hojas gruesas.",
+        "recomendaciones": "Luz media, suelo bien drenado."
+      },
+      {
+        "nombre": "Croton",
+        "frecuenciaRiego": 72,
+        "descripcion": "Planta con hojas de colores brillantes.",
+        "recomendaciones": "Luz brillante, mantener el suelo húmedo."
+      },
+      {
+        "nombre": "Anthurium",
+        "frecuenciaRiego": 96,
+        "descripcion": "Planta tropical con flores en forma de corazón.",
+        "recomendaciones": "Luz brillante indirecta, alta humedad."
+      },
+      {
+        "nombre": "Dracaena",
+        "frecuenciaRiego": 120,
+        "descripcion": "Planta de interior alta con hojas delgadas y largas.",
+        "recomendaciones": "Poca luz, suelo ligeramente húmedo."
+      },
+      {
+        "nombre": "Maranta (Planta de la Oración)",
+        "frecuenciaRiego": 48,
+        "descripcion": "Planta conocida por sus hojas que se pliegan por la noche.",
+        "recomendaciones": "Luz indirecta, alta humedad."
+      },
+      {
+        "nombre": "Aspidistra (Planta de Hierro)",
+        "frecuenciaRiego": 168,
+        "descripcion": "Planta robusta con hojas grandes y verdes.",
+        "recomendaciones": "Poca luz, suelo seco."
+      },
+      {
+        "nombre": "Ficus Lyrata (Árbol de Hoja de Violín)",
+        "frecuenciaRiego": 120,
+        "descripcion": "Árbol de interior con hojas grandes en forma de violín.",
+        "recomendaciones": "Luz brillante indirecta, no tolera el exceso de riego."
+      },
+      {
+        "nombre": "Schefflera (Árbol Paraguas)",
+        "frecuenciaRiego": 120,
+        "descripcion": "Planta con hojas que crecen en un patrón radial.",
+        "recomendaciones": "Luz indirecta, dejar secar el suelo entre riegos."
+      },
   ]
-  
+export default plantsData;
