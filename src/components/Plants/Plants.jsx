2cada608803fbf4d89fafc18b51c276ec8665ce2
Diego Morales <diegomorales1359@gmail.com>
2023-11-29 08:16:44 -0600

    test

--- src/components/Plants/Plants.jsx
+++ src/components/Plants/Plants.jsx
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
+
 export const Plants = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [selectedPlants, setSelectedPlants] = useState([]);
 
-  // Datos de prueba
+  // Datos de prueba ampliados
   const mockData = [
-    'Rosa',
-    'Tulipán',
-    'Girasol',
-    'Orquídea',
-    'Margarita',
-    'Clavel'
+    'Rosa', 'Tulipán', 'Girasol', 'Orquídea', 'Margarita', 'Clavel',
+    'Lavanda', 'Azalea', 'Begonia', 'Cala', 'Dalia', 'Ébano',
+    'Fresia', 'Geranio', 'Hortensia', 'Iris', 'Jazmín', 'Kalanchoe',
+    'Lirio', 'Magnolia', 'Narciso', 'Olivo', 'Petunia', 'Quinoa',
+    'Rododendro', 'Salvia', 'Tomillo', 'Uva', 'Verbena', 'Wisteria',
+    'Xerochrysum', 'Yucca', 'Zinnia', 'Aloe', 'Bonsái', 'Cactus',
+    'Drácena', 'Eucalipto', 'Ficus', 'Ginkgo', 'Helecho', 'Ivy',
+    'Junípero', 'Kiri', 'Laurel', 'Mimosa', 'Nogal', 'Olmo',
+    'Pino', 'Quercus', 'Roble', 'Secuoya', 'Tejo', 'Ulmo',
+    'Violeta', 'Weigela', 'Xanthoceras', 'Yew', 'Zamia'
   ];
 
   const searchPlants = (searchQuery) => {
-    // Filtra los datos de prueba para simular una búsqueda
     const filteredData = mockData.filter(plant =>
       plant.toLowerCase().includes(searchQuery.toLowerCase())
     );
     setResults(filteredData);
+  };
+
+  const addPlant = (plant) => {
+    setSelectedPlants(prevSelectedPlants => [...prevSelectedPlants, plant]);
   };
 
   useEffect(() => {
@@ -29,33 +38,38 @@ export const Plants = () => {
       } else {
         setResults([]);
       }
-    }, 500); // Tiempo de espera de 500ms
+    }, 500);
 
-    return () => clearTimeout(timerId); // Limpia el temporizador al desmontar
+    return () => clearTimeout(timerId);
   }, [query]);
-  return (
-    <>
-      <div className='index-container'>
-        <div className='data-container'>
-          <div className='text-container'>
-            <h1>Plants</h1>
-          </div>
-          <div className='form-container'>
-            <input
-              type="text"
-              placeholder="Buscar plantas..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            />
-            <div>
-              {results.map((plant, index) => (
-                <div key={index}>{plant}</div>
-              ))}
-            </div>
-          </div>
-        </div>
 
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar plantas..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <div>
+        {results.map((plant, index) => (
+          <div key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            {plant}
+            <button onClick={() => addPlant(plant)}>+</button>
+          </div>
+        ))}
       </div>
-    </>
-  )
-}
+      <div>
+        <h3>Plantas seleccionadas:</h3>
+        <ul>
+          {selectedPlants.map((plant, index) => (
+            <li key={index}>{plant}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default PlantSearch;
+
